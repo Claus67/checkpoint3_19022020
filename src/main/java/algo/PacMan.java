9a4426447e22c158eb1dc3ba243b5bf9a5c7c850
Denis Kutlovci <deniskutlovci@hotmail.com>
2020-02-19 11:43:13 +0100

    Quiz done and PacMan Alg.

--- src/main/java/algo/PacMan.java
+++ src/main/java/algo/PacMan.java
@@ -12,8 +12,34 @@ public class PacMan {
      * @param level, eg: 2
      * @return score, eg: 224
      */
+
+
     public static int play(char[][] grid, int level) {
 
-        return 0;
+        int score = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+
+            for (int j = 0; j < grid[i].length; j++) {
+
+                switch (grid[i][j]){
+
+                    case ' ' : score += 0;
+                            break;
+
+                    case '.' : score += 1;
+                            break;
+
+                    case 'o' : score +=10;
+                            break;
+
+                    case 'x' : score += (100 * level);
+                            break;
+                }
+            }
+
+        }
+
+        return score;
     }
 }
