9485570558fde8131cced1b886a49eeb5ad80c24
RS-F <rsf.sea@gmail.com>
2020-02-19 11:09:37 +0100

    Cipher completed

--- src/main/java/algo/Cipher.java
+++ src/main/java/algo/Cipher.java
@@ -8,7 +8,20 @@ public class Cipher {
      * @return encrypted sentence, eg : "wdfrv"
      */
     public static String caesar(String sentence, int shift) {
-
-        return null;
+    	System.out.println("Sentence: " + sentence + ", shift: " + shift);
+    	sentence.toLowerCase();
+    	String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    	String resultString = "";
+    	Character tmpChr;
+    	int charPointer = 0;
+    	for (int i = 0; i < sentence.length(); i++) {
+    		tmpChr = sentence.charAt(i);
+			charPointer = alphabet.indexOf(tmpChr);
+			System.out.println("Character: " + tmpChr + " ,Pointer: " + charPointer);
+			if (charPointer + shift < 0) charPointer += 26;
+			resultString += alphabet.charAt((shift + charPointer) % 26);
+		}
+    	System.out.println("Result: " +resultString);
+        return resultString;
     }
 }
