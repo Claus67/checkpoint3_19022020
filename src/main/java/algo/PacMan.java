105df107eeb92bd5e10e808ff65a59431df36509
mkrill <michael.krill@telekom.de>
2020-02-19 09:47:11 +0100

    solution for PacMan

--- src/main/java/algo/PacMan.java
+++ src/main/java/algo/PacMan.java
@@ -2,18 +2,34 @@ package algo;
 
 public class PacMan {
 
-    /**
-     *
-     * @param grid, eg: {
-     *     {'o', '.', '.'},
-     *     {' ', 'x', ' '},
-     *     {'.', '.', 'o'}
-     * }
-     * @param level, eg: 2
-     * @return score, eg: 224
-     */
-    public static int play(char[][] grid, int level) {
+	/**
+	 *
+	 * @param grid, eg: {
+	 *     {'o', '.', '.'},
+	 *     {' ', 'x', ' '},
+	 *     {'.', '.', 'o'}
+	 * }
+	 * @param level, eg: 2
+	 * @return score, eg: 224
+	 */
 
-        return 0;
-    }
+	public static int play(char[][] grid, int level) {
+
+		int points = 0;
+		for (int i=0; i < grid.length; i++) {
+			for (int j=0; j < grid[i].length; j++) {
+
+				switch (grid[i][j]) {
+				case '.': points++; 
+				break;
+				case 'o': points += 10;
+				break;
+				case 'x': points += 100*level;
+				break;
+				}     			
+			}
+		}
+
+		return points;
+	}
 }
