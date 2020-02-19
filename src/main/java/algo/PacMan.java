336c303ff5e562f0bf36bb89f1ceeba4297c05c8
RS-F <rsf.sea@gmail.com>
2020-02-19 10:29:04 +0100

    Pac Man completed

--- src/main/java/algo/PacMan.java
+++ src/main/java/algo/PacMan.java
@@ -13,7 +13,29 @@ public class PacMan {
      * @return score, eg: 224
      */
     public static int play(char[][] grid, int level) {
-
-        return 0;
+    	
+    	char tmpChar = ' ';
+    	int sum = 0;
+    	System.out.println("Level: " + level);
+    	for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				System.out.println(grid[i][j]);
+				switch (grid[i][j]) {
+				case '.':
+					sum += 1;
+					break;
+				case 'o':
+					sum += 10;
+					break;
+				case 'x':
+					sum += 100 * level;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+    	System.out.println("Summe: " + sum);
+        return sum;
     }
 }
