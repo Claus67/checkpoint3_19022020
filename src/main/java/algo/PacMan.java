4de6833042929b5026cf84a73bcecaf60202b706
Steffen Klaski <steffen.klaski@t-systems.com>
2020-02-19 10:04:32 +0100

    PacMan

--- src/main/java/algo/PacMan.java
+++ src/main/java/algo/PacMan.java
@@ -2,18 +2,29 @@ package algo;
 
 public class PacMan {
 
-    /**
-     *
-     * @param grid, eg: {
-     *     {'o', '.', '.'},
-     *     {' ', 'x', ' '},
-     *     {'.', '.', 'o'}
-     * }
-     * @param level, eg: 2
-     * @return score, eg: 224
-     */
-    public static int play(char[][] grid, int level) {
-
-        return 0;
-    }
+	/**
+	 *
+	 * @param grid,  eg: { {'o', '.', '.'}, {' ', 'x', ' '}, {'.', '.', 'o'} }
+	 * @param level, eg: 2
+	 * @return score, eg: 224
+	 */
+	public static int play(char[][] grid, int level) {
+		int points = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				switch (Character.valueOf((grid[i][j]))) {
+				case 46:
+					points += 1;
+					break;
+				case 111:
+					points += 10;
+					break;
+				case 120:
+					points += (100 * level);
+					break;
+				}
+			}
+		}
+		return points;
+	}
 }
