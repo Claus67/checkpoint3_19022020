f779a6f6c041ffea71ca6a2aeaeb0daf56a407d5
Michael Obst <michael.obst@telekom.de>
2020-02-19 10:51:57 +0100

    algo Pac-Man finished

--- src/main/java/algo/PacMan.java
+++ src/main/java/algo/PacMan.java
@@ -13,7 +13,25 @@ public class PacMan {
      * @return score, eg: 224
      */
     public static int play(char[][] grid, int level) {
+    	int score = 0;
 
-        return 0;
+    	for (int i=0; i<grid.length; i++) {
+    		for (int j=0; j<grid[i].length; j++) {
+    			switch (grid[i][j]) {
+    			case '.':
+    				score += 1;
+    				break;
+    			case 'o':
+    				score += 10;
+    				break;
+    			case 'x':
+    				score += (100 * level);
+    				break;
+    			}
+    			
+    		}
+    	}
+
+        return score;
     }
 }
