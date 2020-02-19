f77fa1686fb4c2b6aff0a896b43e77541256378d
marcelkamp1 <marcelkamp1@gmx.de>
2020-02-19 10:02:02 +0100

    Cipher done

--- src/main/java/algo/Cipher.java
+++ src/main/java/algo/Cipher.java
@@ -1,14 +1,31 @@
 package algo;
 
 public class Cipher {
-
-    /**
+	
+	/**
      * @param sentence, eg : "tacos"
      * @param shift,    eg : 3
      * @return encrypted sentence, eg : "wdfrv"
      */
     public static String caesar(String sentence, int shift) {
 
-        return null;
+    	String encrypted = "";
+    	char abc;
+    	
+    	for(int i = 0; i < sentence.length(); i++) {
+    		abc = sentence.charAt(i);
+    		
+    		if(abc >= 'a' && abc <= 'z') {
+    			abc = (char) (abc + shift);
+    			if(abc > 'z') {
+    				abc = (char) (abc+'a'-'z'-1);
+    			}
+    			if(abc < 'a') {
+    				abc = (char) (abc-'a'+'z'+1);
+    			}
+    		encrypted = encrypted + abc;
+    		}
+    	}
+    	return encrypted;
     }
 }
