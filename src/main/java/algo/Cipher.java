e3c44a0b3db132b789a9d01dbdfae1b45b7e1bb9
Michael Obst <michael.obst@telekom.de>
2020-02-19 11:30:29 +0100

    algo Pac-Man finished

--- src/main/java/algo/Cipher.java
+++ src/main/java/algo/Cipher.java
@@ -1,5 +1,6 @@
 package algo;
 
+
 public class Cipher {
 
     /**
@@ -8,7 +9,19 @@ public class Cipher {
      * @return encrypted sentence, eg : "wdfrv"
      */
     public static String caesar(String sentence, int shift) {
-
-        return null;
+    	
+    	String result = "";
+    	for (int i=0; i<sentence.length(); i++) {
+    		char zeichen = sentence.charAt(i);
+    		zeichen += shift;
+    		if ((int)zeichen < 97) {
+    			zeichen = (char)(123-(97-(int)zeichen));
+    		}
+    		if ((int)zeichen > 122) {
+    			zeichen = (char)(96+(int)zeichen-122);
+    		}
+    		result = result + zeichen;
+    	}
+        return result;
     }
 }
