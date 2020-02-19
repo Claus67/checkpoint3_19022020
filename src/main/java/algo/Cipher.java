d553fcdcf358bf7ce0e1b13cd2dbd7c8315aa1ad
mkrill <michael.krill@telekom.de>
2020-02-19 10:14:16 +0100

    Cipher solution without Sysout

--- src/main/java/algo/Cipher.java
+++ src/main/java/algo/Cipher.java
@@ -2,33 +2,32 @@ package algo;
 
 public class Cipher {
 
-	 public static void main(String[] args) {
-		 
+	public static void main(String[] args) {
+
 		String sentence = "zebra";
 		int shift = 2;
 		System.out.println(sentence + " => " + caesar(sentence, shift));
 	}
-	
-	
-	/**
-     * @param sentence, eg : "tacos"
-     * @param shift,    eg : 3
-     * @return encrypted sentence, eg : "wdfrv"
-     */
-    public static String caesar(String sentence, int shift) {
 
-    	String result = "";
-    	for (int position=0; position < sentence.length(); position++) {
-    		int shiftedValue = sentence.charAt(position) + shift;
-    		if (shiftedValue > 'z') {
-    			shiftedValue = shiftedValue - 'z' + 'a' - 1; 
-    		} else if (shiftedValue < 'a') {
-    			shiftedValue = shiftedValue + 'z' - 'a' +1;     			
-    		}
-    		result += (char) shiftedValue;
-    		
-    		System.out.println(sentence.charAt(position) + " => " + (char) shiftedValue);
-    	}
-        return result;
-    }
+
+	/**
+	 * @param sentence, eg : "tacos"
+	 * @param shift,    eg : 3
+	 * @return encrypted sentence, eg : "wdfrv"
+	 */
+	public static String caesar(String sentence, int shift) {
+
+		String result = "";
+		for (int position=0; position < sentence.length(); position++) {
+			int shiftedValue = sentence.charAt(position) + shift;
+			if (shiftedValue > 'z') {
+				shiftedValue = shiftedValue - 'z' + 'a' - 1; 
+			} else if (shiftedValue < 'a') {
+				shiftedValue = shiftedValue + 'z' - 'a' +1;     			
+			}
+			result += (char) shiftedValue;
+
+		}
+		return result;
+	}
 }
